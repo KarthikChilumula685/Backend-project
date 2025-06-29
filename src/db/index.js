ae96a8d1f1c5efde8e145ca86215c20aa71f22da
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-06-29 22:00:25 +0530

    connected to mongoDB atlas

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongo DB connected to !! DB host ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.error("ERROR: ",error)
+    }
+}
+
+export default connectDB
