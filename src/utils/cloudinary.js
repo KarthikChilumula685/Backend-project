4e721bc748d4f653fd7f35f81dad442662e5894d
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-07-01 17:21:25 +0530

    all setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,20 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUSINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUSINARY_API_KEY, 
+        api_secret: process.env.CLOUSINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = cloudinary.uploader.upload(localFilePath,{ resource_type: "auto" }) 
+        console.log("File uploaded on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
